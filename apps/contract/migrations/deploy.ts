7aa161f0ece1ab5c80473e31a682e752e602b605
Vincent Rainaud <vrainaud.pro@gmail.com>
2025-07-18 15:29:57 +0200

    feat(marketplace): adding tests, fixing marketplace and orders

--- apps/contract/migrations/deploy.ts
+++ apps/contract/migrations/deploy.ts
@@ -1,5 +1,12 @@
-import { type AnchorProvider, setProvider } from '@coral-xyz/anchor';
+// Migrations are an early feature. Currently, they're nothing more than this
+// single deploy script that's invoked from the CLI, injecting a provider
+// configured from the workspace's Anchor.toml.
 
-module.exports = (provider: AnchorProvider) => {
-  setProvider(provider);
+import * as anchor from "@coral-xyz/anchor";
+
+module.exports = async function (provider: anchor.AnchorProvider) {
+  // Configure client to use the provider.
+  anchor.setProvider(provider);
+
+  // Add your deploy script here.
 };
